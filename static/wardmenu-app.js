cebb031446e97a9f616971bf505ef497c746578f
AJ ONeal <coolaj86@gmail.com>
2013-01-06 01:29:24 +0000

    ready to test hosted on github and thewardmenu

--- static/wardmenu-app.js
+++ static/wardmenu-app.js
@@ -1,1 +1,12 @@
-alert('hey, I work!');
+/*jshint strict:true browser:true jquery:true es5:true
+onevar:true laxcomma:true laxbreak:true eqeqeq:true immed:true latedef:true undef:true unused:true*/
+(function () {
+  "use strict";
+
+  $('<link rel="stylesheet" type="text/css" href="http://thewardmenu.com/widget.css" media="screen" />')
+    .appendTo('head');
+  if (!$('#js-wm-root').length) {
+    $('body').append('<div id="js-wm-root"></div>');
+  }
+  $('#js-wm-root').load("http://thewardmenu.com/widget.html");
+}());
--- static/wardmenu-bookmarklet.js
+++ static/wardmenu-bookmarklet.js
@@ -3,7 +3,7 @@
     var wardmenuJs = document.createElement("script");
     /* TODO put on thewardmenu.com with https */
     /* wardmenuJs.src = "http://thewardmenu.com/wardmenu-app.js"; */
-    wardmenuJs.src = "https://gist.github.com/raw/281972c6db467628e776/wardmenu-app.js";
+    wardmenuJs.src = "https://raw.github.com/coolaj86/wardmenu/master/static/wardmenu-app.js";
     wardmenuJs.id = "js-wardmenu-script";
     document.body.appendChild(wardmenuJs);
   }
